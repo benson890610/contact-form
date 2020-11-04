0c2aa0bac3441515251b096b3a57b9636eba6d7a
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-11-04 20:55:42 +0100

    Created: application structure

---
+++ README.md
@@ -0,0 +1,9 @@
+# PHP OOP Contact form
+
+Create contact form, enable clients to contact host via their email.
+
+## Techniques used
+
+- Object oriented programing
+- PHPMailer library
+- Bootstrap
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatabile" content="IE=Edge">
+    <meta name="author" content="Igor Djurdjic">
+    <meta name="description" content="Contact form, Email, PHPMailer">
+    <meta name="keywords" content="contact, igor djurdjic, phpmailer, email">
+    <title>Contact Form | Igor Djurdjic</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    
+    
+
+    <script type="text-javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
