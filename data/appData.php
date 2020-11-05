fa0fbe7f19835f1f03314938e2c561d4d41e5167
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-11-05 21:49:45 +0100

    Added: database and mail credentials

---
+++ data/appData.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Database credentials
+    define("DB_DRIVER", "__YOUR_DATABASE_CONNECTION_DRIVER_DATA__");
+    define("DB_HOST",   "__YOUR_HOST__");
+    define("DB_USER",   "__YOUR_USERNAME__");
+    define("DB_PASS",   "__YOUR_PASSWORD__");
+    define("DB_NAME",   "__YOUR_DATABASE_NAME__");
+    define("DB_CHAR",   "__YOUR_DATABASE_CHARSET__");
+
+    // PHPMailer credentials
+    define("MAIL_HOST",        "__YOUR_EMAIL_POP_HOST");
+    define("MAIL_USERNAME",    "__YOUR_EMAIL_ADDRESS");
+    define("MAIL_PASS",        "__YOUR_MAIL_PASSWORD__");
+    define("MAIL_RECIVE_PORT", "__YOUR_MAIL_PORT_INTEGER__");
---
+++ data/contacts_table.sql
@@ -0,0 +1,36 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: phpsample
+-- ------------------------------------------------------
+-- Server version	8.0.21-0ubuntu0.20.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contacts` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contacts`
+--
