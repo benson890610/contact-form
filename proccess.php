db02fe390cb1cae6ce2a5dbc20c61aa092122545
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-11-05 18:06:00 +0100

    Modified: GET request forbidden, only proccess POST request

--- proccess.php
+++ proccess.php
@@ -1,3 +1,23 @@
 <?php
 
-    
+    ini_set('display_errors', 'on');
+
+    if($_SERVER["REQUEST_METHOD"] === "POST") {
+
+        session_start();
+
+        require "helpers/sessionHelper.php";
+
+        spl_autoload_register(function($className){
+            if(file_exists("library/classes/" . $className . ".php")) {
+                require "library/classes/" . $className . ".php";
+            }
+        });
+
+        $form = new FormHandler;
+        $form->proccessContact();
+
+    } else {
+        header("Location: index.php");
+        exit;
+    }
