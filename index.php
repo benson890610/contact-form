8800f4bffeb2196a92fc949632d9680a37d825c9
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-11-05 14:49:29 +0100

    Updated: UI Contact form created

--- index.php
+++ index.php
@@ -12,8 +12,49 @@
 </head>
 <body>
     
-    
+    <header>
+        <h1 class="text-center mt-5">Contact Form</h1>
+        <p class="lead text-muted text-center">Complete the form below and send message to the app host.</p>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-md-8 col-lg-8 col-sm-12 mx-auto">
+
+                    <div class="card">
+                        <div class="card-body bg-light">
+                            <form action="proccess.php" method="post">
+                                <div class="form-group">
+                                    <label for="email">Your email address</label>
+                                    <input type="text" name="email" id="email" class="form-control">
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="subject">Subject</label>
+                                    <input type="text" name="subject" id="subject" class="form-control">
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                                <div class="form-group">
+                                    <label for="message">Message</label>
+                                    <textarea name="message" id="summary-ckeditor" rows="10" class="form-control"></textarea>
+                                    <span class="invalid-feedback"></span>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">Send</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <div style="margin-top: 200px"></div>
 
     <script type="text-javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="ckeditor/ckeditor.js"></script>
+    <script>CKEDITOR.replace( 'summary-ckeditor' );</script>
 </body>
 </html>
