98233fc818499c3dbf76282d093a8a0d68a73fd0
Igor Djurdjic <igor.djurdjic89@gmail.com>
2020-11-05 18:08:46 +0100

    Modified: Validation class finished

---
+++ library/classes/Validation.php
@@ -0,0 +1,44 @@
+<?php
+
+    class Validation {
+
+        private $validationOk = true;
+
+        const MIN_CHAR_LEN = 5;
+
+        public function contactRequest(Request $request) {
+            $this->validEmail($request->getEmail());
+            $this->validSubject($request->getSubject());
+            $this->validMessage($request->getMessage());
+            
+            return $this->validationOk;
+        }
+        private function validEmail($text) {
+            if(empty($text)) {
+                $this->validationOk = false;
+                setSessionError("email", "Email address required");
+            } else {
+                if(filter_var($text, FILTER_VALIDATE_EMAIL) === FALSE) {
+                    $this->validationOk = false;
+                    setSessionError("email", "Invalid email address");
+                }
+            }
+        }
+        private function validSubject($text) {
+            if(empty($text)) {
+                $this->validationOk = false;
+                setSessionError("subject", "Subject field required");
+            } else {
+                if(strlen($text) < self::MIN_CHAR_LEN) {
+                    $this->validationOk = false;
+                    setSessionError("subject", "Subject field require minimum 5 character length");
+                }
+            }
+        }
+        private function validMessage($text) {
+            if(empty($text)) {
+                $this->validationOk = false;
+                setSessionError("message", "Message field is required");
+            }
+        }
+    }
